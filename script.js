3216e78ad649775bc8f4241fb6e00fcd22dcb0ed
Dani <dmmraymundo1@gmail.com>
2023-11-08 18:03:15 +0800

    Modify password validation logic

--- script.js
+++ script.js
@@ -17,36 +17,47 @@ form.addEventListener("submit", (e) => {
 });
 
 function validateInputs() {
+    let allFieldsValid = true;
+
     if (firstName.value.trim().length === 0) {
         makeInvalid(firstName, "First name is required");
+        allFieldsValid = false;
     } else if (!isFormatValid(firstName.value, firstNamePattern)) {
         makeInvalid(firstName, "Only letters are allowed");
+        allFieldsValid = false;
     } else {
         makeValid(firstName);
     };
 
     if (lastName.value.trim().length === 0) {
         makeInvalid(lastName, "Last name is required");
+        allFieldsValid = false;
     } else if (!isFormatValid(lastName.value, lastNamePattern)) {
         makeInvalid(lastName, "Only letters are allowed");
+        allFieldsValid = false;
     } else {
         makeValid(lastName);
     };
 
     if (email.value.trim().length === 0) {
         makeInvalid(email, "Email is required");
+        allFieldsValid = false;
     } else if (!isFormatValid(email.value, emailPattern)) {
         makeInvalid(email, "Please enter a valid email address");
+        allFieldsValid = false;
     } else {
         makeValid(email);
     };
 
     if (phone.value.trim().length === 0) {
         makeInvalid(phone, "Phone number is required");
+        allFieldsValid = false;
     } else if (phone.value.trim().length < 4) {
         makeInvalid(phone, "Please enter a valid phone number");
+        allFieldsValid = false;
     } else if (!isFormatValid(phone.value, phonePattern)) {
         makeInvalid(phone, "Please enter a valid phone number");
+        allFieldsValid = false;
     } else {
         makeValid(phone);
     };
@@ -54,19 +65,34 @@ function validateInputs() {
     if (pw.value.trim().length === 0) {
         makeInvalid(pw, "Password is required");
         makeInvalid(confirm, " ");
+        allFieldsValid = false;
     } else if (pw.value.trim().length < 8) {
         makeInvalid(pw, "Password must be at least 8 characters");
         makeInvalid(confirm, " ");
+        allFieldsValid = false;
     } else if (!isFormatValid(pw.value, pwPattern)) {
         makeInvalid (pw, "Password must contain at least 1 uppercase letter, lowercase letter, digit, and special character");
         makeInvalid(confirm, " ");
+        allFieldsValid = false;
+    } else if (pw.value.trim().length > 0 && 
+                pw.value.trim().length >= 8 && 
+                isFormatValid(pw.value, pwPattern) && 
+                confirm.value.trim().length === 0) {
+        makeValid(pw);
+        makeInvalid(confirm, "Please retype your password")
+        allFieldsValid = false
     } else if (pw.value.trim() !== confirm.value.trim()) {
         makeInvalid(confirm, "Passwords do not match");
         makeInvalid(pw,"");
+        allFieldsValid = false;
     } else {
         makeValid(confirm);
         makeValid(pw);
     };
+
+    if(allFieldsValid) {
+        form.submit();
+    }
 };
 
 function isFormatValid(input, pattern) {
